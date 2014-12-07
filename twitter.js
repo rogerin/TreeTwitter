8054360b9aaf004e93a895d715fc294db28d058c
Rogerio Alencar Filho <geriofilho@gmail.com>
2014-12-07 15:25:56 -0300

    remove secret key

--- twitter.js
+++ twitter.js
@@ -1,35 +1,39 @@
 var Twitter = require('node-tweet-stream')
   , t = new Twitter({
-    consumer_key: 'gS7SNA6Vbl5Y6YVabNg9OFVwT',
-    consumer_secret: 'j6hfOF8xOtmyOj0HcIL4OBMCjSXFdCqsYP6huDAinGeEQp3xSC',
-    token: '25347187-etaDV7xBi2t4LQQm1JM7iDLwinp3oPiQ8zyXDyWmn',
-    token_secret: '99lYSCn8BEG8VAzEKAoW1EmaNrxWsZuF6XJGq9dyuXvCv'
+    consumer_key: 'SEU-KEY-AQUI',
+    consumer_secret: 'SEU-KEY-AQUI',
+    token: 'SEU-KEY-AQUI',
+    token_secret: 'SEU-KEY-AQUI'
   });
 
-t.on('tweet', function (tweet) {
 
-  console.log('========================================================================');
-  console.log('========================================================================');
-  console.log('========================================================================');
-  console.log('========================================================================');
-  console.log('========================================================================');
+t.on('tweet', function (tweet) {
   console.log('========================================================================');
   console.log('========================================================================');
 
   //console.log('tweet received', tweet);
+  //console.log('tweet received', tweet.text);
 
-  console.log('tweet received', tweet.text);
-
-
-  console.log('========================================================================');
-  console.log('========================================================================');
-  console.log('========================================================================');
-  console.log('========================================================================');
-  console.log('========================================================================');
-  console.log('========================================================================');
-  console.log('========================================================================');
+  console.log('Original: ', tweet.text);
   
+  var texto = tweet.text.toLowerCase();
+  console.log('Minuscula: ', texto);
 
+  var n = texto.indexOf("#");
+  console.log('N: ', n);
+
+  var res = texto.substring(n, n+8);
+  console.log('res:', res);
+
+  if(res.toString() == '#treeoff') {
+    console.log('Desliga!');
+    // implementar GPIO para desligar
+  } else if (res.toString() == '#treeon') {
+    console.log('Ligar!');
+    // implementar GPIO para ligar
+  } else {
+    console.log('faz nada, :/ ');
+  }
 
 });
 
@@ -38,6 +42,9 @@ t.on('error', function (err) {
 });
 
 t.track('@rogerin')
+t.track('#treeon')
+t.track('#treeoff')
+
 
 // 10 minutes later
 //t.untrack('pizza')
